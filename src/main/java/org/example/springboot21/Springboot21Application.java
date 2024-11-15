474bb963cedd57a05a4ce29690e391e5aadcb615
clt <1906746908@qq.com>
2024-11-15 14:35:26 +0800

    initial commit

--- src/main/java/org/example/springboot21/Springboot21Application.java
+++ src/main/java/org/example/springboot21/Springboot21Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springboot21Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot21Application.class, args);
+        SpringApplication.run(Springboot21Application.class , args);
     }
 
 }
